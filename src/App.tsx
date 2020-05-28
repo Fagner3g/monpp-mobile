b3809e9ef5d1f7937c2f92ad8d6928be3b1994f5
Fagner Gomes <fagner.egomes@gmail.com>
2020-05-27 23:13:37 -0300

    style: 💄 We configure the fonts
    Fonts RobotoSlab

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Text } from 'react-native'
 
-const App: React.FC<> = () => <Text>Olá mundo</Text>
+const App: React.FC = () => <Text>Olá mundo</Text>
 
 export default App
