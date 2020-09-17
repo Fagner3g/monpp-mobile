450e191d08bf3a37c4b9876f8d65b6a0b77d6388
Fagner Egidio Gomes <fagner.gomes@compasso.com.br>
2020-09-16 22:25:31 -0300

    feat: 🎸 Configurando preload valida token
    configuração do preload para validação do token

--- src/pages/Preload/index.tsx
+++ src/pages/Preload/index.tsx
@@ -1,9 +1,27 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Text } from 'react-native';
+import AsyncStorage from '@react-native-community/async-storage';
+import { useNavigation } from '@react-navigation/native';
 
 import { Container, LoadingIcon } from './styles';
 
 const Preload: React.FC = () => {
+  const navigation = useNavigation();
+
+  useEffect(() => {
+    const checkToken = async () => {
+      const token = await AsyncStorage.getItem('token');
+
+      if (token !== null) {
+        // Validar o token
+      } else {
+        navigation.navigate('SignIn');
+      }
+    };
+
+    checkToken();
+  }, []);
+
   return (
     <Container>
       <Text>Preload</Text>
